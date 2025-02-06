695eae23c6e5f3fadcad2f8b41aaccf9b3955f46
Drew Crawford <drew@sealedabstract.com>
2025-02-06 00:27:56 -0600

    Update docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 /*!
 
-World's tiniest TGA encoder.
+This is the world's simplest way to write images from Rust in the TGA format. It is one single file and has no dependencies.
 
-This is a small library for writing TGA images.  Designed for dumping GPU textures to disk.
+If you are looking for a drop-dead simple way to write images to open in your image editor, and have a tab open on BMP and TIFF formats right now trying to figure out which one you can emit using a for loop, this is the crate you are looking for. TGA is a widely supported but trivial file format. This library supports BGRA 32-bit uncompressed images only, a popular and trivial subset.
 
-The library supports writing 32-bit BGRA textures; other formats may be supported in the future.
+I designed this library to dump GPU textures for debugging purposes, but it is suitable for a wide variety of applications.
 
 */
 #[repr(C)]
